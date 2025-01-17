1667abe033427e3ab6bf811c9f880c6dfcc7bcab
folhesgabriel <pisagabriel09@gmail.com>
2025-01-17 16:22:46 -0300

    feat: add custom logo and css

--- src/ckanext-codata/ckanext/codata/plugin.py
+++ src/ckanext-codata/ckanext/codata/plugin.py
@@ -7,10 +7,15 @@ class CodataPlugin(plugins.SingletonPlugin):
     
 
     # IConfigurer
-
+    #NOTE: update_config is the interface for the plugin config
     def update_config(self, config_):
+        #a toolkit function to tell CKAN where are the plugin view templates
         toolkit.add_template_directory(config_, "templates")
+        #a toolkit function to tell CKAN where are the plugin public assets such as JS, CSS, and images.
         toolkit.add_public_directory(config_, "public")
+        #a toolkit function that helps us to add assets libraries for our plugin. We use this to create our WebAssets for CSS and JS assets.
         toolkit.add_resource("assets", "codata")
+        
+        
         
         
