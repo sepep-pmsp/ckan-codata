a7922a0b4419b1c68be5b84b34e8c94194662dde
folhesgabriel <pisagabriel09@gmail.com>
2025-11-02 18:29:17 +0000

    fix setup scripts

---
+++ ckan/docker-entrypoint.d/02_setup_codata.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing ckanext-codata"
+pip install -e /srv/app/src_extensions/ckanext-codata
--- ckan/docker-entrypoint.d/03_setup_codata.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Installing ckanext-codata"
-pip install -e /srv/app/src_extensions/ckanext-codata
-
-echo "Adding ckanext-codata to CKAN__PLUGINS"
-export CKAN__PLUGINS="$CKAN__PLUGINS codata"
